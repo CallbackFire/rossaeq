15cdb7c3d3ab7cc13bebfc17fc0d1eddd9c92ead
lemba <wildfirezad@gmail.com>
2017-05-18 16:18:06 +0300

    added boilerplate function for admin users actiions deleting ,adding and updating

--- application/controllers/dashboard_controller/Dashboard_controller.php
+++ application/controllers/dashboard_controller/Dashboard_controller.php
@@ -5,13 +5,44 @@
 
 class Dashboard_controller extends CI_Controller {
 
-	public function __construct(){
+	public function __construct()
+	{
 		parent::__construct();
 		//check for login
 	}
 	
-	public function index(){
+	public function index()
+	{
 		$this->load->view('dashboard/index');
+	}
+
+	public function login()
+	{
+
+	}
+
+	public function logout()
+	{
+
+	}
+
+	public function add_for_rent($post_data)
+	{
+
+	}
+
+	public function delete_for_rent($property_id)
+	{
+		//change view status of the propertyforrent
+	}
+
+	public function add_for_sale($post_data)
+	{
+		
+	}
+	public function delete_for_sale($property_id)
+	{
+
 	}
 }
 
