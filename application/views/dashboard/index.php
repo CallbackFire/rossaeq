e3c4367fa351b1c204db2f9f85d8d5c98933776b
lemba <wildfirezad@gmail.com>
2017-05-20 17:48:16 +0300

    okay ...gotta move kapuka just called ...plus im bored :(

--- application/views/dashboard/index.php
+++ application/views/dashboard/index.php
@@ -98,7 +98,7 @@
               <ul class="nav navbar-nav navbar-right">
                 <li class="">
                   <a href="javascript:;" class="user-profile dropdown-toggle" data-toggle="dropdown" aria-expanded="false">
-                    <img src="images/img.jpg" alt="">John Doe
+                    <img src="<?php echo base_url() ?>uploads/admin_profiles/one.jpg" alt=""> Francis
                     <span class=" fa fa-angle-down"></span>
                   </a>
                   <ul class="dropdown-menu dropdown-usermenu pull-right">
